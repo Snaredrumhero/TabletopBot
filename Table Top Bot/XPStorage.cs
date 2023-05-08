e8bb5014409f1c0139649dabd7488d984cfa88a2
KyleChiasson <89480424+KyleChiasson@users.noreply.github.com>
2023-05-08 18:17:45 -0400

    Implemented Raffle

--- Table Top Bot/XPStorage.cs
+++ Table Top Bot/XPStorage.cs
@@ -7,86 +7,82 @@
         {
             { "Temp", 0 }, //just temp
         };
-        public struct User
+        readonly static int[] PointsToTickets = new int[]
         {
-            public string discordID;
-            public List<Game> gamesPlayed;
-            public bool wonRaffle; //Could be changed to an int if wanted multiple wins
-            public bool[] achievements; //true for each achievement achieved
+            0, //need to add more ticket values
+        };
+        public class User
+        {
+            public string discordID = "";
+            public string PID = "";
+            public List<Game> gamesPlayed = new List<Game>();
+            public bool wonRaffle = false; //Could be changed to an int if wanted multiple wins
+            public bool[] achievements = new bool[Achievements.Count]; //true for each achievement achieved
+            public uint numberGamesPlayed = 0; //only used for tracking game ids
+            public static bool operator > (User _a, User _b) { return GetUserPointValue(_a) > GetUserPointValue(_b); }
+            public static bool operator <(User _a, User _b) { return GetUserPointValue(_a) < GetUserPointValue(_b); }
         }
-        public struct Game
+        public class Game
         {
-            enum GameType { NULL = 0, Ranked, CoOp, Unranked }
-            GameType type;
-            uint playerCount;
-            uint rank;
-            float length;
+            public uint id = 0;
+            public enum GameType { NULL = 0, Ranked, CoOp, Unranked }
+            public GameType type = GameType.NULL;
+            public uint playerCount = 0;
+            public uint rank = 0;
+            public float length = 0.0f;
         }
 
-        public static void InitUser(string _discordID)
+        public static User? GetUser(string _discordID)
+        {
+            User? u = null;
+            for (int i = 0; i < Users.Count; i++)
+                if (Users[i].discordID == _discordID)
+                    u = Users[i];
+            return u;
+        }
+        public static void InitUser(string _discordID, string _pid)
         {
             for (int i = 0; i < Users.Count; i++)
                 if (Users[i].discordID == _discordID)
                     return;
-            Users.Add(new User
-            {
-                discordID = _discordID,
-                gamesPlayed = new List<Game>(),
-                wonRaffle = false,
-                achievements = new bool[Achievements.Count],
-            });;
+            Users.Add(new User { discordID = _discordID, PID = _pid });
         }
-        public static void RemoveUser(string _discordID)
+        public static void RemoveUser(User _user) { Users.Remove(_user); }
+        public static void ClaimAchivement(User _user, string _achievementName)
         {
-            for (int i = 0; i < Users.Count; i++)
-                if (Users[i].discordID == _discordID)
+            if (!Achievements.ContainsKey(_achievementName))
+                return;
+            _user.achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = true;
+        }
+        public static void UnclaimAchievement(User _user, string _achievementName)
+        {
+            if (!Achievements.ContainsKey(_achievementName))
+                return;
+            _user.achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = false;
+        }
+        public static void UserAddGame(User _user)
+        {
+            /*_user.gamesPlayed.Add(new Game
+            {
+                id = u.numberGamesPlayed,
+                type = ,
+                playerCount = ,
+                rank = ,
+                length = ,
+            });*/
+            _user.numberGamesPlayed++;
+        }
+        public static void UserRemoveGame(User _user, int _id)
+        {
+            for (int i = 0; i < _user.gamesPlayed.Count; i++)
+                if (_user.gamesPlayed[i].id == _id)
                 {
-                    Users.RemoveAt(i);
+                    _user.gamesPlayed.RemoveAt(i);
                     return;
                 }
         }
-        public static void ClaimAchivement(string _discordID, string _achievementName)
+        public static int GetUserPointValue(User _user)
         {
-            if (!Achievements.ContainsKey(_achievementName))
-                return;
-            User? u = null;
-            for (int i = 0; i < Users.Count; i++)
-                if (Users[i].discordID == _discordID)
-                    u = Users[i];
-            if (u == null)
-                return;
-            u.Value.achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = true;
-        }
-        public static void UnclaimAchievement(string _discordID, string _achievementName)
-        {
-            if (!Achievements.ContainsKey(_achievementName))
-                return;
-            User? u = null;
-            for (int i = 0; i < Users.Count; i++)
-                if (Users[i].discordID == _discordID)
-                    u = Users[i];
-            if (u == null)
-                return;
-            u.Value.achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = false;
-        }
-        public static void UserAddGame()
-        {
-
-        }
-        public static void UserRemoveGame()
-        {
-
-        }
-        public static int GetUserPointValue(string _discordID)
-        {
-            //Verrifies user is in system
-            User? u = null;
-            for (int i = 0; i < Users.Count; i++)
-                if (Users[i].discordID == _discordID)
-                    u = Users[i];
-            if (u == null)
-                return 0;
-
             int points = 0;
 
             //Adds for each game played
@@ -95,21 +91,34 @@
             //Adds for each achievement
             int[] achievementValues = Achievements.Values.ToArray();
             for (int i = 0; i < achievementValues.Length; i++)
-                if (u.Value.achievements[i])
+                if (_user.achievements[i])
                     points += achievementValues[i];
             return points;
         }
-        public static string DisplayUser(string _discordID)
+        public static string DisplayUser(User _user) { return $"{_user.discordID}\nPID: {_user.PID}\nPoints: {GetUserPointValue(_user)}"; }
+        public static string DrawRaffle() //returns the message to send to the server
         {
-            return "";
-        }
-        public static void DrawRaffle()
-        {
-
+            List<int> raffleIDs = new List<int>();
+            for (int i = 0; i < Users.Count; i++)
+                if (!Users[i].wonRaffle)
+                {
+                    int p = GetUserPointValue(Users[i]);
+                    for (int j = 0; j < PointsToTickets.Length; j++)
+                        if (PointsToTickets[i] <= p)
+                            raffleIDs.Add(i);
+                }
+            Random r = new Random(DateTime.Now.Minute + DateTime.Now.Second + DateTime.Now.Millisecond);
+            return $"@Everyone Congrats to {Users[raffleIDs[r.Next(raffleIDs.Count)]]} for winning the raffle!";
         }
         public static string DisplayTopXUsers(int _x)
         {
-            return "";
+            if (_x > Users.Count)
+                return "";
+            Users.Sort();
+            string s = "";
+            for (int i = 0; i < _x; i++)
+                s += $"{i}: {GetUserPointValue(Users[i])} - {Users[i].discordID}\n";
+            return s;
         }
     }
 }
