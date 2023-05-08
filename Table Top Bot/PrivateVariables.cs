2d19dff933bb169f43942e149ba184637de2734e
KyleChiasson <89480424+KyleChiasson@users.noreply.github.com>
2023-05-08 08:55:14 -0400

    Removed the ignored files

--- Table Top Bot/PrivateVariables.cs
+++
@@ -1,8 +0,0 @@
-﻿//Don't share this file
-namespace Table_Top_Bot
-{
-    public class PrivateVariables
-    {
-        public const string KEY = "MTA0ODMwMTYxMjIyNzEwMDc1NA.GQNt4V.ulHFuwiPZcN5kSQI4H2d9GtD63OPPHg6bNvr9k";
-    }
-}
