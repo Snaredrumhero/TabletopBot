8e004e912cae49ae1a417159f82556437c74e9ab
KyleChiasson <89480424+KyleChiasson@users.noreply.github.com>
2023-05-08 08:46:39 -0400

    Added Bot Base Files

---
+++ PrivateVariables.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Table_Top_Bot
+{
+    public class PrivateVariables
+    {
+        public const string KEY = "MTA0ODMwMTYxMjIyNzEwMDc1NA.GQNt4V.ulHFuwiPZcN5kSQI4H2d9GtD63OPPHg6bNvr9k";
+    }
+}
---
+++ Table Top Bot/PingPongExample.cs
@@ -0,0 +1,24 @@
+﻿using Discord.WebSocket;
+
+namespace Table_Top_Bot
+{
+    //An example Module
+    internal class PingPong : Module
+    {
+        //Constructor must be here even if there is nothing except base call
+        public PingPong(Program _bot) : base(_bot) { }
+
+        //Place all code that adds a callback here
+        public override void InitilizeModule()
+        {
+            Bot.AddMessageReceivedCallback(MessageReceived);
+        }
+
+        //All functions including callbacks (each callback takes different arguements)
+        private async Task MessageReceived(SocketMessage message)
+        {
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("Pong!");
+        }
+    }
+}
