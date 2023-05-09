1d3024949f3cc18435a90f619dbe264592d8dfc0
KyleChiasson <89480424+KyleChiasson@users.noreply.github.com>
2023-05-09 13:32:29 -0400

    Updated XP Storage Functions to User Functions

--- Table Top Bot/XPStorage.cs
+++ Table Top Bot/XPStorage.cs
@@ -19,8 +19,94 @@
             public bool wonRaffle = false; //Could be changed to an int if wanted multiple wins
             public bool[] achievements = new bool[Achievements.Count]; //true for each achievement achieved
             public uint numberGamesPlayed = 0; //only used for tracking game ids
-            public static bool operator > (User _a, User _b) { return GetUserPointValue(_a) > GetUserPointValue(_b); }
-            public static bool operator <(User _a, User _b) { return GetUserPointValue(_a) < GetUserPointValue(_b); }
+            public static bool operator > (User _a, User _b) { return _a.GetPointValue() > _b.GetPointValue(); }
+            public static bool operator <(User _a, User _b) { return _a.GetPointValue() < _b.GetPointValue(); }
+
+            public string Print() { return $"{discordID}\nPID: {PID}\nPoints: {GetPointValue()}"; }
+            //Game type 1 = ranked, 2 = CoOp, 3 = Teams, 4 = Party
+            //Rank = 1 for win 2 for loss
+            public void UserAddGame(uint _playerCount, uint _gameType, uint _rank, uint _length)
+            {
+                Game.GameType type = Game.GameType.NULL;
+                if (_gameType == 1)
+                    type = Game.GameType.Ranked;
+                else if (_gameType == 2)
+                    type = Game.GameType.CoOp;
+                else if (_gameType == 3)
+                    type = Game.GameType.Teams;
+                else if (_gameType == 4)
+                    type = Game.GameType.Party;
+                if (type == Game.GameType.NULL)//error
+                    return;
+                gamesPlayed.Add(new Game
+                {
+                    id = numberGamesPlayed,
+                    type = type,
+                    playerCount = _playerCount,
+                    rank = _rank,
+                    length = _length,
+                });
+                numberGamesPlayed++;
+            }
+            public void UserRemoveGame(int _id)
+            {
+                for (int i = 0; i < gamesPlayed.Count; i++)
+                    if (gamesPlayed[i].id == _id)
+                    {
+                        gamesPlayed.RemoveAt(i);
+                        return;
+                    }
+            }
+            public int GetPointValue()
+            {
+                int points = 0;
+
+                //Adds for each game played
+                for (int i = 0; i < gamesPlayed.Count; i++)
+                    points += XPCalc(gamesPlayed[i]);
+
+                //Adds for each achievement
+                int[] achievementValues = Achievements.Values.ToArray();
+                for (int i = 0; i < achievementValues.Length; i++)
+                    if (achievements[i])
+                        points += achievementValues[i];
+                return points;
+            }
+            public void ClaimAchivement(string _achievementName)
+            {
+                if (Achievements.ContainsKey(_achievementName))
+                    achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = true;
+            }
+            public void UnclaimAchievement(string _achievementName)
+            {
+                if (Achievements.ContainsKey(_achievementName))
+                    achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = false;
+            }
+
+            //XP stuff
+            private const float POINTS_SCALE = 1.5f;//scale of points awarded (makes numbers bigger bc big numbers are fun)
+            private static readonly float[] RANKED_POSITIONS = { 24f / 24f, 22f / 24f, 20f / 24f, 18f / 24f, 17f / 24f, 16f / 24f };
+            private static int XPCalc(Game _game)
+            {
+                float points = POINTS_SCALE * _game.length * 3 / 2;
+                if (_game.type == Game.GameType.NULL || _game.rank <= 0 || _game.rank > _game.playerCount)//error
+                    return 0;
+                if (_game.type == Game.GameType.Ranked)
+                    if (_game.playerCount >= RANKED_POSITIONS.Length)
+                    {
+                        if (_game.rank > RANKED_POSITIONS.Length)
+                            return (int)(points * RANKED_POSITIONS[RANKED_POSITIONS.Length - 1]);
+                        return (int)(points * RANKED_POSITIONS[_game.rank - 1]);
+                    }
+                    else
+                        //1st gets full (24/24)
+                        //if 3 people 2 gets 20
+                        //if 5 people 2: 22 3:20 4:18
+                        //last gets 2/3 (16/24)
+                        return (int)(points * (8f * (_game.playerCount - _game.rank) / (_game.playerCount - 1 + 16) / 24));
+                //CoOp Teams Party W: 3rd place L: 2/3 last
+                return (int)(points * (_game.rank == 1 ? RANKED_POSITIONS[2] : RANKED_POSITIONS[RANKED_POSITIONS.Length - 1]));
+            }
         }
         public class Game
         {
@@ -39,76 +125,14 @@
                 return;
             Users.Add(new User { discordID = _discordID, PID = _pid });
         }
-        public static void RemoveUser(User _user) { Users.Remove(_user); }
-        public static void ClaimAchivement(User _user, string _achievementName)
-        {
-            if (!Achievements.ContainsKey(_achievementName))
-                return;
-            _user.achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = true;
-        }
-        public static void UnclaimAchievement(User _user, string _achievementName)
-        {
-            if (!Achievements.ContainsKey(_achievementName))
-                return;
-            _user.achievements[Achievements.Keys.ToList().IndexOf(_achievementName)] = false;
-        }
-        //Game type 1 = ranked, 2 = CoOp, 3 = Teams, 4 = Party
-        //Rank = 1 for win 2 for loss
-        public static void UserAddGame(User _user, uint _playerCount, uint _gameType, uint _rank, uint _length)
-        {
-            Game.GameType type = Game.GameType.NULL;
-            if (_gameType == 1)
-                type = Game.GameType.Ranked;
-            else if (_gameType == 2)
-                type = Game.GameType.CoOp;
-            else if (_gameType == 3)
-                type = Game.GameType.Teams;
-            else if (_gameType == 4)
-                type = Game.GameType.Party;
-            if (type == Game.GameType.NULL)//error
-                return;
-            _user.gamesPlayed.Add(new Game
-            {
-                id = _user.numberGamesPlayed,
-                type = type,
-                playerCount = _playerCount,
-                rank = _rank,
-                length = _length,
-            });
-            _user.numberGamesPlayed++;
-        }
-        public static void UserRemoveGame(User _user, int _id)
-        {
-            for (int i = 0; i < _user.gamesPlayed.Count; i++)
-                if (_user.gamesPlayed[i].id == _id)
-                {
-                    _user.gamesPlayed.RemoveAt(i);
-                    return;
-                }
-        }
-        public static int GetUserPointValue(User _user)
-        {
-            int points = 0;
-
-            //Adds for each game played
-            for (int i = 0; i < _user.gamesPlayed.Count; i++)
-                points += XPCalc(_user.gamesPlayed[i]);
-
-            //Adds for each achievement
-            int[] achievementValues = Achievements.Values.ToArray();
-            for (int i = 0; i < achievementValues.Length; i++)
-                if (_user.achievements[i])
-                    points += achievementValues[i];
-            return points;
-        }
-        public static string DisplayUser(User _user) { return $"{_user.discordID}\nPID: {_user.PID}\nPoints: {GetUserPointValue(_user)}"; }
+        public static void RemoveUser(User _user) => Users.Remove(_user);
         public static string DrawRaffle() //returns the message to send to the server
         {
             List<int> raffleIDs = new List<int>();
             for (int i = 0; i < Users.Count; i++)
                 if (!Users[i].wonRaffle)
                 {
-                    int p = GetUserPointValue(Users[i]);
+                    int p = Users[i].GetPointValue();
                     for (int j = 0; j < PointsToTickets.Length; j++)
                         if (PointsToTickets[i] <= p)
                             raffleIDs.Add(i);
@@ -123,32 +147,8 @@
             Users.Sort();
             string s = "";
             for (int i = 0; i < _x; i++)
-                s += $"{i}: {GetUserPointValue(Users[i])} - {Users[i].discordID}\n";
+                s += $"{i}: {Users[i].GetPointValue()} - {Users[i].discordID}\n";
             return s;
-        }
-
-        private const float POINTS_SCALE = 1.5f;//scale of points awarded (makes numbers bigger bc big numbers are fun)
-        private static readonly float[] RANKED_POSITIONS = { 24f/24f, 22f/24f, 20f/24f, 18f/24f, 17f/24f, 16f/24f };
-        private static int XPCalc(Game _game)
-        {
-            float points = POINTS_SCALE * _game.length * 3 / 2;
-            if (_game.type == Game.GameType.NULL || _game.rank <= 0 || _game.rank > _game.playerCount)//error
-                return 0;
-            if(_game.type == Game.GameType.Ranked)
-                if (_game.playerCount >= RANKED_POSITIONS.Length)
-                {
-                    if(_game.rank > RANKED_POSITIONS.Length)
-                        return (int)(points * RANKED_POSITIONS[RANKED_POSITIONS.Length - 1]);
-                    return (int)(points * RANKED_POSITIONS[_game.rank - 1]);
-                }
-                else
-                    //1st gets full (24/24)
-                    //if 3 people 2 gets 20
-                    //if 5 people 2: 22 3:20 4:18
-                    //last gets 2/3 (16/24)
-                    return (int)(points * (8f * (_game.playerCount - _game.rank) / (_game.playerCount - 1 + 16) / 24));
-            //CoOp Teams Party W: 3rd place L: 2/3 last
-            return (int)(points * (_game.rank == 1 ? RANKED_POSITIONS[2] : RANKED_POSITIONS[RANKED_POSITIONS.Length - 1]));
         }
     }
 }
